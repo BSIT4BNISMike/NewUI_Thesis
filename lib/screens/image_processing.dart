21f24d858734ce5740cf0b86477cd4f90b442632
BSIT4BNISMike <janmichael.alaestante@wvsu.edu.ph>
2023-05-11 21:53:18 +0800

    Update image_processing.dart

--- lib/screens/image_processing.dart
+++ lib/screens/image_processing.dart
@@ -38,8 +38,8 @@ class _ImageProcessingState extends State<ImageProcessing> {
         path: image.path,
         numResults:
             3, //the amount of categories our neural network can predict (here no. of Body Types)
-        threshold: 0.3,
-        imageMean: 0.0,
+        threshold: 0.5,
+        imageMean: 127.5,
         imageStd: 127.5,
       );
       setState(() {
